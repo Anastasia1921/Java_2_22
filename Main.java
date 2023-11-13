0c2ecd82c5244c2a37bf8680ee46d4963b9227d6
Anastasia1921 <150111215+Anastasia1921@users.noreply.github.com>
2023-11-13 08:41:40 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -67,15 +67,138 @@ public class Main {
                     calendar.set(y+1900, m, d);
                     int numWorkDays2 = 0;
                     int newId = 0;
-                    while (calendar.before(newCalendar)) {
-                        newId = calendar.get(Calendar.DAY_OF_WEEK);
+
+                    Calendar Calendar1 ;
+                    Calendar Calendar2 ;
+                    int reverse;
+                    if (calendar.before(newCalendar))
+                    {
+                        Calendar1 = calendar;
+                        Calendar2 = newCalendar;
+                        reverse = 1;
+                    }
+                    else {
+                        Calendar2 = calendar;
+                        Calendar1 = newCalendar;
+                        reverse = -1;
+                    }
+
+                    while (Calendar1.before(Calendar2)) {
+                        newId = Calendar1.get(Calendar.DAY_OF_WEEK);
                         if (!((newId == Calendar.SUNDAY) || (newId == Calendar.SATURDAY))) {
                             numWorkDays2++;
                         }
-                        calendar.add(Calendar.DAY_OF_YEAR, 1);
+                        Calendar1.add(Calendar.DAY_OF_YEAR, 1);
                     }
 
-                    System.out.println(numWorkDays2);
+                    System.out.println(numWorkDays2 * reverse);
+                }
+                else {
+                    System.out.println("новая строка пуста");
+                }
+            } else {
+                System.out.println("строка пуста");
+            }
+        } catch (ParseException e) {
+            System.out.println("строку не удалось распознать");
+        }
+    }
+}
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+        //1
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите строку с датой формата 31.12.2020");
+        String strDate = in.nextLine();
+        try {
+            if (!strDate.isEmpty()) {
+                SimpleDateFormat fmtDate = new SimpleDateFormat("dd.MM.yyyy");
+                Date prsDate = fmtDate.parse(strDate);
+
+                //2
+                int y = prsDate.getYear();
+                int m = prsDate.getMonth();
+                int d = prsDate.getDate();
+                Calendar calendar = new GregorianCalendar(1900+y, m, d);
+
+                calendar.add(Calendar.DAY_OF_YEAR, 45);
+                System.out.println(fmtDate.format(calendar.getTime()));
+
+                //3
+                int m2 = 0; // месяцы начинаются с 0
+                int d2 = 1;
+                int y2 = calendar.get(Calendar.YEAR);
+                calendar.set(Calendar.DAY_OF_MONTH,d2);
+                calendar.set(Calendar.MONTH,m2);
+                System.out.println(fmtDate.format(calendar.getTime()));
+
+                //4
+                int numWorkDays = 0;
+                int id = 0;
+                while (numWorkDays < 10) {
+
+                    id = calendar.get(Calendar.DAY_OF_WEEK);
+                    if (!((id==Calendar.SUNDAY)||(id==Calendar.SATURDAY))) {
+                        numWorkDays++;
+                    }
+                    if (numWorkDays< 10) calendar.add(Calendar.DAY_OF_YEAR, 1);
+                }
+
+                id = calendar.get(Calendar.DAY_OF_WEEK);
+                if (id==Calendar.SUNDAY) {
+                    calendar.add(Calendar.DAY_OF_YEAR, 1);
+                }
+                else if (id==Calendar.SATURDAY) {
+                    calendar.add(Calendar.DAY_OF_YEAR, 2);
+                }
+                System.out.println(fmtDate.format(calendar.getTime()));
+
+                //5
+                System.out.println("введите новую строку с датой формата 31.12.2020");
+                String strNewDate = in.nextLine();
+                if (!strNewDate.isEmpty()) {
+                    Date prsNewDate = fmtDate.parse(strNewDate);
+
+                    //6
+                    int newY = prsNewDate.getYear();
+                    int newM = prsNewDate.getMonth();
+                    int newD = prsNewDate.getDate();
+                    Calendar newCalendar = new GregorianCalendar(1900 + newY, newM, newD);
+
+                    calendar.set(y+1900, m, d);
+                    int numWorkDays2 = 0;
+                    int newId = 0;
+
+                    Calendar Calendar1 ;
+                    Calendar Calendar2 ;
+                    int reverse;
+                    if (calendar.before(newCalendar))
+                    {
+                        Calendar1 = calendar;
+                        Calendar2 = newCalendar;
+                        reverse = 1;
+                    }
+                    else {
+                        Calendar2 = calendar;
+                        Calendar1 = newCalendar;
+                        reverse = -1;
+                    }
+
+                    while (Calendar1.before(Calendar2)) {
+                        newId = Calendar1.get(Calendar.DAY_OF_WEEK);
+                        if (!((newId == Calendar.SUNDAY) || (newId == Calendar.SATURDAY))) {
+                            numWorkDays2++;
+                        }
+                        Calendar1.add(Calendar.DAY_OF_YEAR, 1);
+                    }
+
+                    System.out.println(numWorkDays2 * reverse);
                 }
                 else {
                     System.out.println("новая строка пуста");
